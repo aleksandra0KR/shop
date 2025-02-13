f0150c5ac9db442e8107393ac7879c69a28bc563
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2025-02-13 16:57:57 +0300

    fix: change models

--- domain/merch.go
+++ domain/merch.go
@@ -1,9 +1,7 @@
 package domain
 
-import "github.com/shopspring/decimal"
-
 type Merch struct {
-	Guid  string          `json:"guid" gorm:"column:guid;primaryKey"`
-	Name  string          `json:"name" gorm:"column:name;not null"`
-	Price decimal.Decimal `json:"price" gorm:"column:price;type:decimal(20,8);not null"`
+	Guid  string  `json:"guid" gorm:"column:guid;primaryKey"`
+	Name  string  `json:"name" gorm:"column:name;not null;unique"`
+	Price float64 `json:"price" gorm:"column:price;type:decimal(20,8);not null"`
 }
--- domain/transaction.go
+++ domain/transaction.go
@@ -1,16 +1,15 @@
 package domain
 
 import (
-	"github.com/shopspring/decimal"
 	"time"
 )
 
 type Transaction struct {
-	Guid         string          `json:"guid" gorm:"column:guid;primaryKey"`
-	CreatedAt    time.Time       `json:"created_at" gorm:"column:created_at;autoCreateTime"`
-	ReceiverGUID string          `json:"receiver_guid" gorm:"column:receiver_guid;not null"`
-	Receiver     User            `json:"-" gorm:"foreignKey:ReceiverGUID;references:Guid"`
-	SenderGUID   string          `json:"sender_guid" gorm:"column:sender_guid;not null"`
-	Sender       User            `json:"-" gorm:"foreignKey:SenderGUID;references:Guid"`
-	MoneyAmount  decimal.Decimal `json:"money_amount" gorm:"column:money_amount;type:decimal(20,8);not null"`
+	Guid         string    `json:"guid" gorm:"column:guid;primaryKey"`
+	CreatedAt    time.Time `json:"created_at" gorm:"column:created_at;autoCreateTime"`
+	ReceiverGUID string    `json:"receiver_guid" gorm:"column:receiver_guid;not null"`
+	Receiver     User      `json:"-" gorm:"foreignKey:ReceiverGUID;references:Guid"`
+	SenderGUID   string    `json:"sender_guid" gorm:"column:sender_guid;not null"`
+	Sender       User      `json:"-" gorm:"foreignKey:SenderGUID;references:Guid"`
+	MoneyAmount  float64   `json:"money_amount" gorm:"column:money_amount;type:decimal(20,8);not null"`
 }
