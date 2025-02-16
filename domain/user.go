9c016efaecb5e157d459d675bee1384107e96db1
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2025-02-16 03:16:18 +0300

    fix: change fields names and delete unused fields

--- domain/user.go
+++ domain/user.go
@@ -5,8 +5,7 @@ import (
 )
 
 type User struct {
-	GUID        string    `json:"guid" gorm:"column:guid;primaryKey"`
-	Username    string    `json:"username" gorm:"column:username;unique;not null"`
+	Username    string    `gorm:"column:username;primaryKey"`
 	Password    string    `json:"-" gorm:"column:password;not null"`
 	Balance     float64   `json:"balance" gorm:"column:balance;type:decimal(20,8);default:1000"`
 	CreatedAt   time.Time `json:"created_at" gorm:"column:created_at;autoCreateTime"`
